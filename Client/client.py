7ac5e1f3fd50a46577f4c1f8b231ccf0e95b8890
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-06-25 22:40:50 +0530

    Add files via upload

---
+++ Client/client.py
@@ -0,0 +1,30 @@
+#python 3.7.1
+import threading
+import socket
+alias = input('choose an alias>>> ')
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('127.0.0.1',10245))
+def client_receive():
+    while True:
+      try:
+         message = client.recv(1024).decode('utf-8')
+         if message == "alias?":
+             client.send(alias.encode('utf-8'))
+         else:
+             print(message)
+             if message == 'exit(10240)':
+                 break
+             else:
+                 pass
+      except:
+            print("Error!")
+            client.close()
+            break
+def client_send():
+    while True:
+      message = f'{alias}:{input("")}'
+      client.send(message.encode('utf-8'))
+receive_thread = threading.Thread(target = client_receive) 
+receive_thread.start()
+send_thread = threading.Thread(target = client_send)
+send_thread.start()
